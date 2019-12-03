f03f45a30e4767e279c1791f09a9a69b89c3fb85
fengxiaopei <xpfeng>
2019-12-03 15:20:12 +0800

    initial commit

---
+++ ckit/strings.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+#include "strings.h"
+
+#define ERROR_BUFSIZE 64
+__thread char tls_error_buf[ERROR_BUFSIZE];
+
+const char* ckit_error(int err_code) {
+  int n = strerror_r(err_code, tls_error_buf, ERROR_BUFSIZE);
+  if (n == 0) {
+    return tls_error_buf;
+  } else {
+    snprintf(tls_error_buf, ERROR_BUFSIZE, "Unknown error %d", err_code);
+    return tls_error_buf;
+  }
+}
---
+++ ckit/strings.h
@@ -0,0 +1,6 @@
+#ifndef CKIT_STRINGS_H
+#define CKIT_STRINGS_H
+
+const char* ckit_error(int err_code);
+
+#endif
